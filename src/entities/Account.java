f2f135ed9e78604ebd3c94267edb73277bd4799b
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-10-27 16:58:33 -0300

    construtor account e get and seters

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -3,7 +3,43 @@ package entities;
 public class Account {
 	
 	private Integer number;
-	private String holver;
+	private String holder;
 	private Double balance;
+	
+	public Account() {
+		
+	}
 
+	public Account(Integer number, String holder, Double balance) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	
 }
