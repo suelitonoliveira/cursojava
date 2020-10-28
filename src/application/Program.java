7ee48899c40e55f9338e5de82f00ca7b9044dbe7
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-10-28 09:00:18 -0300

    Herança, upcasting e downcasting

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,40 @@
 package application;
 
+import entities.Account;
+import entities.BussinessAccount;
+import entities.SavingAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
-
+		Account acc = new Account(1001, "Alex", 0.0);
+		BussinessAccount bacc = new BussinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		// upcasting
+		
+		Account acc1 = bacc;
+		Account acc2 = new BussinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
+		
+		// downcasting
+		
+		BussinessAccount acc4 = (BussinessAccount)acc2;
+		acc4.loan(100.0);
+		
+	
+		// verifica se é instancia de BussinessAccount
+		if (acc3 instanceof BussinessAccount) {
+			BussinessAccount acc5 = (BussinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof SavingAccount) {
+			SavingAccount acc5 = (SavingAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 	}
 
 }
--- src/entities/Account.java
+++ src/entities/Account.java
@@ -4,14 +4,13 @@ public class Account {
 	
 	private Integer number;
 	private String holder;
-	private Double balance;
+	protected Double balance;
 	
 	public Account() {
 		
 	}
 
 	public Account(Integer number, String holder, Double balance) {
-		super();
 		this.number = number;
 		this.holder = holder;
 		this.balance = balance;
@@ -37,9 +36,12 @@ public class Account {
 		return balance;
 	}
 
-	public void setBalance(Double balance) {
-		this.balance = balance;
+	public void withdraw(double amount) {
+		balance -= amount;
 	}
 
+	public void deposit(double amount) {
+		balance += amount;
+	}
 	
 }
