8ee09ac8294d854aaba46658efd34e6dba50bf2d
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-10-28 13:11:53 -0300

    Classes abstratas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,51 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
+import entities.BussinessAccount;
 import entities.SavingAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingAccount(1023, "Maria", 1000.0, 0.01);
+		List<Account> list = new ArrayList<>();
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		list.add(new SavingAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BussinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingAccount(1004, "Bob", 300.0, 0.01));
+		list.add(new BussinessAccount(1005, "Maria", 500.0, 500.0));
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		double  sum = 0.0;
+		
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		
+		for (Account acc : list) {
+			acc.deposit(10.0);
+		}
+		
+		for (Account acc : list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
+		
+		/*
+		 * Account x = new Account(1020, "Alex", 1000.0); Account y = new
+		 * SavingAccount(1023, "Maria", 1000.0, 0.01);
+		 * 
+		 * x.withdraw(50.0); y.withdraw(50.0);
+		 * 
+		 * System.out.println(x.getBalance()); System.out.println(y.getBalance());
+		 */
 		
 		/*
 		 * Account acc = new Account(1001, "Alex", 1000.0); acc.withdraw(200.00);
