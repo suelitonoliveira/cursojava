9535732d2cc79a66bba9f5868f37fba5b57f7dba
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-11-09 18:54:59 -0300

    Trabalhando com arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,84 +1,28 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import entities.Account;
-import entities.BussinessAccount;
-import entities.SavingAccount;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		
-		List<Account> list = new ArrayList<>();
-		
-		list.add(new SavingAccount(1001, "Alex", 500.00, 0.01));
-		list.add(new BussinessAccount(1002, "Maria", 1000.0, 400.0));
-		list.add(new SavingAccount(1004, "Bob", 300.0, 0.01));
-		list.add(new BussinessAccount(1005, "Maria", 500.0, 500.0));
-		
-		double  sum = 0.0;
-		
-		for (Account acc : list) {
-			sum += acc.getBalance();
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		
-		System.out.printf("Total balance: %.2f%n", sum);
-		
-		
-		for (Account acc : list) {
-			acc.deposit(10.0);
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+			
 		}
-		
-		for (Account acc : list) {
-			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
-		}
-		
-		/*
-		 * Account x = new Account(1020, "Alex", 1000.0); Account y = new
-		 * SavingAccount(1023, "Maria", 1000.0, 0.01);
-		 * 
-		 * x.withdraw(50.0); y.withdraw(50.0);
-		 * 
-		 * System.out.println(x.getBalance()); System.out.println(y.getBalance());
-		 */
-		
-		/*
-		 * Account acc = new Account(1001, "Alex", 1000.0); acc.withdraw(200.00);
-		 * System.out.println(acc.getBalance());
-		 * 
-		 * Account acc2 = new SavingAccount(1002, "maria", 1000.0, 0.01);
-		 * acc2.withdraw(200.0); System.out.println(acc2.getBalance());
-		 * 
-		 * 
-		 * Account acc3 = new BussinessAccount(1003, "Bob", 1000.0, 500.0);
-		 * acc3.withdraw(200.0); System.out.println(acc3.getBalance());
-		 */
-		
-		
-		
-		/*
-		 * // upcasting
-		 * 
-		 * Account acc1 = bacc; Account acc2 = new BussinessAccount(1003, "Bob", 0.0,
-		 * 200.0); Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
-		 * 
-		 * // downcasting
-		 * 
-		 * BussinessAccount acc4 = (BussinessAccount)acc2; acc4.loan(100.0);
-		 * 
-		 * 
-		 * // verifica se é instancia de BussinessAccount if (acc3 instanceof
-		 * BussinessAccount) { BussinessAccount acc5 = (BussinessAccount)acc3;
-		 * acc5.loan(200.0); System.out.println("Loan!"); }
-		 * 
-		 * if (acc3 instanceof SavingAccount) { SavingAccount acc5 =
-		 * (SavingAccount)acc3; acc5.updateBalance(); System.out.println("Update!"); }
-		 */
 	}
 		
 
---
+++ src/application/Program2.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.io.FileReader;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		String path = "c:\\temp\\in.txt";
+		FileReader fr = null;
+		
+
+	}
+
+}
